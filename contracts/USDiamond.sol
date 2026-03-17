f8f69b89da56be394b643cf105f8c66bc38745af
USDiamond <rehandoni532@gmail.com>
2026-03-17 19:13:21 +0700

    Create USDiamond.sol

---
+++ contracts/USDiamond.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDiamond is ERC20, Ownable {
+
+    constructor() ERC20("USDiamond", "USDX") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    // Mint new tokens (backed by diamonds off-chain)
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Burn tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
